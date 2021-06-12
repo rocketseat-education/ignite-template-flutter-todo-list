7067faeec95afaa022940e57451ca604b1e0f77c
Paulo Victor <paulo.victor158@gmail.com>
2021-06-12 05:28:35 -0400

    creating a initial AppWidget with a blank HomeScreen

---
+++ lib/app_widget.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Container(),
+      ),
+    );
+  }
+}
