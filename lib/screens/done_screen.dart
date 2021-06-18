b05aa882dc12291bd2a563558b712ee49847e92d
Paulo Victor <paulo.victor158@gmail.com>
2021-06-18 19:08:28 -0400

    refactoring done screen to pass functions through parameter

--- lib/screens/done_screen.dart
+++ lib/screens/done_screen.dart
@@ -4,46 +4,48 @@ import '../shared/models/todo_item.dart';
 import 'components/todo_item_list_tile.dart';
 
 class DoneScreen extends StatefulWidget {
+  const DoneScreen({
+    Key? key,
+    required this.itemList,
+    required this.onResetItem,
+    required this.onRemoveItem,
+  }) : super(key: key);
+
+  final List<ToDoItem> itemList;
+  final ValueChanged<ToDoItem> onResetItem;
+  final ValueChanged<ToDoItem> onRemoveItem;
+
   @override
   _DoneScreenState createState() => _DoneScreenState();
 }
 
 class _DoneScreenState extends State<DoneScreen> {
-  final _toDoItemList = <ToDoItem>[];
-  final _doneItemList = <ToDoItem>[];
-
   @override
   Widget build(BuildContext context) {
-    // appBar
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        SizedBox(height: 24),
+        Text(
+          'Concluídas',
+          textAlign: TextAlign.center,
+          style: TextStyle(color: Colors.blue, fontSize: 24),
+        ),
+        Flexible(
+          child: ListView.builder(
+            itemCount: widget.itemList.length,
+            itemBuilder: (context, index) {
+              final item = widget.itemList[index];
 
-    return Container(
-      height: 500,
-      padding: const EdgeInsets.only(bottom: 32),
-      child: ListView.builder(
-        itemCount: _toDoItemList.length,
-        itemBuilder: (context, index) {
-          final item = _toDoItemList[index];
-          return ToDoItemListTile(
-            item: item,
-            onRemoveItem: () {
-              setState(() {
-                _toDoItemList.remove(item);
-              });
+              return ToDoItemListTile(
+                item: item,
+                onRemoveItem: () => widget.onRemoveItem(item),
+                onChangeItem: () => widget.onResetItem(item),
+              );
             },
-            onCompleteItem: () {
-              setState(() {
-                _toDoItemList.remove(item);
-                _doneItemList.add(
-                  ToDoItem(
-                    title: item.title,
-                    isDone: true,
-                  ),
-                );
-              });
-            },
-          );
-        },
-      ),
+          ),
+        ),
+      ],
     );
   }
 }
