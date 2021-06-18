a4e7b5a0533355b36d2350557b75e971f14bf1e3
Paulo Victor <paulo.victor158@gmail.com>
2021-06-18 19:09:07 -0400

    fully implementing a setState state manager

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'screens/done_screen.dart';
 import 'screens/task_screen.dart';
+import 'shared/models/todo_item.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -9,24 +10,102 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final _toDoItemList = <ToDoItem>[];
+  final _doneItemList = <ToDoItem>[];
+
+  final _pageViewController = PageController(
+    initialPage: 0,
+    keepPage: true,
+  );
 
   var _selectedIndex = 0;
 
-  final _bodyOptions = <Widget>[
-    TaskScreen(),
-    DoneScreen(),
-  ];
+  void onAddItem(String itemTitle) {
+    setState(() {
+      _toDoItemList.add(
+        ToDoItem(
+          title: itemTitle,
+        ),
+      );
+    });
+  }
+
+  void onResetItem(ToDoItem item) {
+    setState(() {
+      _doneItemList.remove(item);
+
+      _toDoItemList.add(
+        ToDoItem(
+          title: item.title,
+        ),
+      );
+    });
+  }
+
+  void onRemoveToDoItem(ToDoItem item) {
+    setState(() {
+      _toDoItemList.remove(item);
+    });
+  }
+
+  void onRemoveDoneItem(ToDoItem item) {
+    setState(() {
+      _doneItemList.remove(item);
+    });
+  }
+
+  void onCompleteItem(ToDoItem item) {
+    setState(() {
+      _toDoItemList.remove(item);
+
+      _doneItemList.add(
+        ToDoItem(
+          title: item.title,
+          isDone: true,
+        ),
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    _pageViewController.dispose();
+
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: _bodyOptions[_selectedIndex],
+      body: PageView(
+        controller: _pageViewController,
+        children: <Widget>[
+          TaskScreen(
+            itemList: _toDoItemList,
+            onAddItem: onAddItem,
+            onCompleteItem: onCompleteItem,
+            onRemoveItem: onRemoveToDoItem,
+          ),
+          DoneScreen(
+            itemList: _doneItemList,
+            onRemoveItem: onRemoveDoneItem,
+            onResetItem: onResetItem,
+          ),
+        ],
+        onPageChanged: (index) {
+          setState(() => _selectedIndex = index);
+        },
       ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _selectedIndex,
         onTap: (index) {
           setState(() => _selectedIndex = index);
+
+          _pageViewController.animateToPage(
+            _selectedIndex,
+            duration: Duration(milliseconds: 350),
+            curve: Curves.easeOut,
+          );
         },
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
